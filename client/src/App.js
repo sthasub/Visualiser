48ec921c4973d8b549153360f08574f222c357ec
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-29 20:35:20 +0930

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { createContext, useReducer } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import $ from "jquery";
+import "react-datepicker/dist/react-datepicker.css";
 import Nav from "./components/Nav";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import AddPatientInfo from "./pages/AddPatientInfo";
+import Registration from "./pages/Registration";
+import {initialState, reducer} from "./reducer/UseReducer";
+
 import 'c3/c3.css';
 
+export const UserContext = createContext();
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div>
+      <UserContext.Provider value={{state, dispatch}}>
       <Router>
       <Nav />
       <Switch>
         <Route exact path="/" component={Dashboard}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/addInformation" component={AddPatientInfo}/>
+        <Route exact path="/registration" component={Registration}/>
         <Redirect to="/" />
       </Switch>
     </Router>
-    
+    </UserContext.Provider>
     </div>
     );
 }
