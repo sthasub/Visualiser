d35e7694c2eaa5e53468cb02a216e55cf01457bf
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-29 20:40:58 +0930

    added button for each diagnosis in List component

--- client/src/components/List/index.jsx
+++ client/src/components/List/index.jsx
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
-import API from "../../utils/API"
-const List = ({handler})=>{
-    const [diseaseName, setDiseaseName] = useState([]);
+import API from "../../utils/API";
+const List = ({ handler }) => {
+  const [diseaseName, setDiseaseName] = useState([]);
 
-    const getDisease = (disease)=>{
-        let putArray = [];
-        disease.map(
-            (disease) => putArray.push(disease.name)
-          );
-        return putArray; 
-    }
+  const getDisease = (disease) => {
+    let putArray = [];
+    disease.map((disease) => putArray.push(disease.name));
+    return putArray;
+  };
 
-    useEffect(()=>{
-        API.getDiagnosis().then(res=>{
-            setDiseaseName(getDisease(res.data));
-            
-        }).catch(err=>{
-            console.log(err);
-        })
-    },[]);
-    
-    return (<>
-        {diseaseName.map((d, index)=>
-                       <div  className="bg-light my-2" id="list" key={index} onClick={()=>handler(d)}>{d.toUpperCase()}</div>
-        )}
-        
-    </>);
-}
+  useEffect(() => {
+    API.getDiagnosis()
+      .then((res) => {
+        setDiseaseName(getDisease(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h4>List of diagnosis</h4>
+      </div>
+      {diseaseName.map((d, index) => (
+        <div
+          className="bg-light my-2"
+          title="click to see the data"
+          id="list"
+          key={index}
+          onClick={() => handler(d)}
+        >
+          {d.toUpperCase()}
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default List;
