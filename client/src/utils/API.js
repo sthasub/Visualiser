5f1bd9a2cb59e62591efd4b452cc5b529e3e62f8
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-29 20:34:58 +0930

    modified API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,10 +4,28 @@ export default {
     getData:function(){
         return axios.get("/user");
     },
+    getAddPatient:function(){
+        return axios.get("/user/userData", { withCredentials: true } );
+    },
+    getPatientData:function(){
+        return axios.get("/patient");
+    },
     getDiagnosis:function(){
         return axios.get("/diagnosis");
     },
-    savePatientInfo:function(patientData){
-        return axios.post("/user", patientData);
+    savePatientInfo:function(patientData, userToken){
+        return axios.put(`/user/patient/${userToken}`, patientData);
+    },
+    saveStaffInfo:function(staffData){
+        return axios.post("/user/signup",staffData);
+    },
+    signIn:function(loginData){
+        return axios.post("/user/login",loginData);
+    },
+    userDetail:function(){
+        return axios.get("/user/userData");
+    },
+    signOut:function(){
+        return axios.get("/user/logout");
     }
 };
