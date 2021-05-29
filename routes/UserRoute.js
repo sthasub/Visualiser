e9fbc4af11e58cae0bf307d511fe81d976891824
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-29 20:25:39 +0930

    modified routes of user and patient

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const withAuth = require("../middleware/withAuth");
+const bcrypt = require("bcryptjs");
 const { User, Disease, Patient } = require("../models");
 
 router.get("/", async (req, res) => {
@@ -19,9 +21,19 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.post("/", async (req, res) => {
-    console.log(req.body);
+router.get("/userData", withAuth, async (req, res) => {
+    const getUserName = await User.findOne({"tokens.token":req.token});
+    res.json({name:getUserName.username, token:req.token});
+})
+
+router.get("/logout", (req, res)=>{
+    res.clearCookie("visualiserToken", {path:"/"});
+    res.status(200).json("logout success!");
+})
+
+router.put("/patient/:token",  async (req, res) => {
     try {
+        const checkToken = await User.findOne({"tokens.token":req.params.token});
         const patient = await Patient.create({
             name: req.body.firstname + " " + req.body.lastname,
             birthYear: req.body.dob,
@@ -36,19 +48,62 @@ router.post("/", async (req, res) => {
 
         const user = await User.findOneAndUpdate(
             {
-                name: "subhash",
-                email: "subahs@email.com",
-                password: "anything"
+                _id:checkToken._id
             }, { $push: { diseases: diagnosis._id } },
             { upsert: true, new: true }
         );
-        console.log(user);
-        res.status(200).send({success:"data saved"});
+        res.status(200).send({ success: "data saved" });
     } catch (error) {
         console.log(error);
         res.status(400).json(error)
     }
-})
+});
 
+router.post("/signup", async (req, res) => {
+        
+    try {
+        const userCheck = await User.findOne({ staffId: req.body.staffId });
+        if (userCheck) {
+            return res.status(422).json({ error: "all ready exist" });
+        }
+        const user = await User.create({
+            username: req.body.firstname + " " + req.body.lastname,
+            staffId: req.body.staffId,
+            email: req.body.email,
+            password: req.body.password,
+            cpassword: req.body.cpassword
+        });
+        res.status(201).json({ success: "data saved" });
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+router.post("/login", async (req, res) => {
+
+    try {
+
+        const userLogin = await User.findOne({ staffId: req.body.staffId });
+
+        if (userLogin) {
+            const isMatch = await bcrypt.compare(req.body.password, userLogin.password);
+            const token = await userLogin.generateToken();
+            res.cookie("visualiserToken", token, {
+                expires: new Date(Date.now() + (60 * 1000 * 60 * 12)),
+                httpOnly: true
+            })
+            if (!isMatch) {
+                return res.status(400).json({ msg: "invalid credential password" });
+            } else {
+                res.status(201).json({ message: "login successful", token:token });
+            }
+        } else {
+            return res.status(400).json({ msg: "invalid credential" });
+        }
+
+    } catch (error) {
+          console.log(error);  
+    }
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoute = require("./UserRoute");
+const patientRoute = require("./PatientRoute");
 const diagnosisRoute = require("./DiagnosisRoute");
 
-router.use("/user",userRoute);
-router.use("/diagnosis",diagnosisRoute);
+router.use("/user", userRoute);
+router.use("/diagnosis", diagnosisRoute);
+router.use("/patient", patientRoute);
 
 module.exports = router;
