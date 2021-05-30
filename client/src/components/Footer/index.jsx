c63f95334af73a1df55b110db9d16ef5d67c826d
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-05-30 13:04:43 +0930

    set margin for footer in login page

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,14 +1,15 @@
 import react from "react";
 import "./style.css";
-
+import {useLocation} from "react-router-dom";
 const Footer = () => {
+    const location = useLocation();
   const getCurrentYear = () => {
     const date = new Date(Date.now());
     return date.getFullYear().toString();
   };
 
   return (
-    <div>
+    <div className={location.pathname === "/login"? "getmargin":""}>
       <br />
       <footer className="bg-primary">
         Copyright &copy; {getCurrentYear()}
